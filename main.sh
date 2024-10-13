57b7111393fe51d1896a007e679667468a83e6f7
RIZZA MUH F <163370562+abrstorenet@users.noreply.github.com>
2024-10-13 15:42:11 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -278,7 +278,7 @@ TEXT="
 <code>Exp Sc : </code><code>$EXPSC</code>
 <code>────────────────────</code>
 <i>Automatic Notification from Github</i>
-"'&reply_markup={"inline_keyboard":[[{"text":"ᴏʀᴅᴇʀ","url":"https://t.me/agungvpnstore"},{"text":"Contack","url":"https://wa.me/6287792681887"}]]}'
+"'&reply_markup={"inline_keyboard":[[{"text":"ᴏʀᴅᴇʀ","url":"https://t.me/agungvpnstore"},{"text":"Contack","url":"https://wa.me/6281235970771"}]]}'
 curl -s --max-time $TIMES -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
 }
 clear
